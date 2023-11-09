53631fec0998ba3f6359511b522f3c2c2a21b2a6
ivaylopp <ivaylo.work.off@gmail.com>
2023-11-09 08:43:33 +0200

    Editorconfig

--- src/app.py
+++ src/app.py
@@ -4,10 +4,10 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return "Hello! I am a Flask application running on {}".format(socket.gethostname())
+	return "Hello! I am a Flask application running on {}".format(socket.gethostname())
 
 
 if __name__ == '__main__':
-    # Note the extra host argument. If we didn't have it, our Flask app
-    # would only respond to requests from inside our container
-    app.run(host='0.0.0.0')
+	# Note the extra host argument. If we didn't have it, our Flask app
+	# would only respond to requests from inside our container
+	app.run(host='0.0.0.0')
